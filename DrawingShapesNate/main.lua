c955169bd3243f97a2a4bc55c7e508c7e7ef27f6
Nate Day <nate.day@ocsbstudent.ca>
2019-09-26 16:19:07 -0400

    Initial commit

---
+++ DrawingShapesNate/main.lua
@@ -0,0 +1,31 @@
+-- Name: Nate Day
+-- Course: ICS2O
+-- This program displays 4 differnt shapes and says their name underneath.
+
+
+-- Hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Add a backround 
+display.setDefault("background", 24/255, 29/255, 49/255)
+
+-- Create a new triangle variables
+local Triangle
+local TriangleText
+local textSize = 75
+
+-- Sets the location of my new triangle and displays it
+Triangle = display.newPolygon( 768, 200, {0, -120, 120,120, -120, 120,})
+
+-- Sets the colour of the triangle
+Triangle:setFillColor(1, 0, 0)
+
+-- Sets the stoke width and colour of the triangle
+Triangle.strokeWidth = 15
+Triangle:setStrokeColor(1, .5, .5)
+
+-- Set the text location to under the triangle
+TriangleText = display.newText("Triangle", 768, 360, (ComicSansMS), textSize)
+
+-- Create new shape
+
