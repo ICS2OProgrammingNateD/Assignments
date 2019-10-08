05e69e8a28a75bb85e6c258fa4e9eed5e21d809a
Nate.Day <nate.day@ocsbstudent.ca>
2019-10-07 22:05:58 -0400

    update

--- AnimatingImagesNate/main.lua
+++ AnimatingImagesNate/main.lua
@@ -1,7 +1,22 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Name: Nate Day
+-- Course: ICS2O
+-- This program displays 4 differnt shapes and says their name underneath.
+-- Animating Images
 
--- Your code here
+-- Hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Add a backround image 
+backgroundImage = display.newImageRect("Images/background.png", 1324, 768)
+backgroundImage.x = display.contentCenterX
+backgroundImage.y = display.contentCenterY
+
+
+---------------------------------------------------------------------------------------------------------
+--LOCAL VARIABLES
+---------------------------------------------------------------------------------------------------------
+
+local backgroundImage
+local yoshi
+local flower
+local mario
