d389f1d7a2b895334b80e0ea984b177eefc7db12
Nate Day <55556056+s445276@users.noreply.github.com>
2019-11-19 14:35:24 -0500

    push

--- ZombieRunNate/main.lua
+++ ZombieRunNate/main.lua
@@ -2,8 +2,8 @@
 --
 -- main.lua
 -- Created by: Nate Day
--- Date: Nov. 16, 2019
--- Description: This calls the main menu of the app to load itself.
+-- Date: Nov. 7th, 2019
+-- Description: This calls the splash screen of the app to load itself.
 -----------------------------------------------------------------------------------------
 
 -- Hiding Status Bar
@@ -18,5 +18,5 @@ local composer = require( "composer" )
 
 -- Go to the intro screen
 
---composer.gotoScene( "main_menu" )
-composer.gotoScene( "main_menu" )
+--composer.gotoScene( "level1_screen" )
+composer.gotoScene( "splash_screen" )
--- ZombieRunNate/main_menu.lua
+++ ZombieRunNate/main_menu.lua
@@ -1,47 +1,33 @@
 -----------------------------------------------------------------------------------------
---
--- credits_screen.lua
--- Created by: Ms Raffin
--- Special thanks to Wal Wal for helping in the design of this framework.
--- Date: Nov. 24th, 2014
--- Description: This is the credits page, displaying a back button.
+-- intro_screen.lua
+-- Created by: Nate Day
+-- Date: Nov. 7th, 2019
+-- Description: This is the main menu of the game. It displays the app logo and the 
+-- company logo with some sort of animation...
 -----------------------------------------------------------------------------------------
 
------------------------------------------------------------------------------------------
--- INITIALIZATIONS
------------------------------------------------------------------------------------------
-
--- Use Composer Libraries
+-- Use Composer Library
 local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Use Widget Library
 local widget = require( "widget" )
 
 -----------------------------------------------------------------------------------------
 
 -- Naming Scene
-sceneName = "instructions_screen"
+sceneName = "main_menu"
+
+-----------------------------------------------------------------------------------------
 
 -- Creating Scene Object
-scene = composer.newScene( sceneName ) -- This function doesn't accept a string, only a variable containing a string
+local scene = composer.newScene( sceneName )
 
 -----------------------------------------------------------------------------------------
 -- LOCAL VARIABLES
 -----------------------------------------------------------------------------------------
-local bkg_image
-local backButton
 
------------------------------------------------------------------------------------------
--- LOCAL FUNCTIONS
------------------------------------------------------------------------------------------
-
--- Creating Transitioning Function back to main menu
-local function BackTransition( )
-    composer.gotoScene( "main_menu", {effect = "slideDown", time = 1000})
-end
-
-
------------------------------------------------------------------------------------------
--- GLOBAL SCENE FUNCTIONS
------------------------------------------------------------------------------------------
 
 -- The function called when the screen doesn't exist
 function scene:create( event )
@@ -50,52 +36,14 @@ function scene:create( event )
     local sceneGroup = self.view
 
     -----------------------------------------------------------------------------------------
-    -- BACKGROUND AND DISPLAY OBJECTS
+    -- BACKGROUND IMAGE & STATIC OBJECTS
     -----------------------------------------------------------------------------------------
 
-    -- Insert the background image and set it to the center of the screen
-    bkg_image = display.newImageRect("Images/Instructions Screen.png", display.contentWidth, display.contentHeight)
-    bkg_image.x = display.contentCenterX
-    bkg_image.y = display.contentCenterY
-    bkg_image.width = display.contentWidth
-    bkg_image.height = display.contentHeight
 
-    -- Associating display objects with this scene 
-    sceneGroup:insert( bkg_image )
 
-    -- Send the background image to the back layer so all other objects can be on top
-    bkg_image:toBack()
+end -- function scene:create( event )   
 
-    -----------------------------------------------------------------------------------------
-    -- BUTTON WIDGETS
-    -----------------------------------------------------------------------------------------
 
-    -- Creating Back Button
-    backButton = widget.newButton( 
-    {
-        -- Setting Position
-        x = display.contentWidth*1/8,
-        y = display.contentHeight*15/16,
-
-        -- Setting Dimensions
-        -- width = 1000,
-        -- height = 106,
-
-        -- Setting Visual Properties
-        defaultFile = "Images/BackButtonUnpressedNate@2x.png",
-        overFile = "Images/BackButtonPressedNate@2x.png",
-
-        -- Setting Functional Properties
-        onRelease = BackTransition
-
-    } )
-
-    -----------------------------------------------------------------------------------------
-
-    -- Associating Buttons with this scene
-    sceneGroup:insert( backButton )
-    
-end --function scene:create( event )
 
 -----------------------------------------------------------------------------------------
 
@@ -111,15 +59,17 @@ function scene:show( event )
 
     -----------------------------------------------------------------------------------------
 
+    -- Called when the scene is still off screen (but is about to come on screen).   
     if ( phase == "will" ) then
-        -- Called when the scene is still off screen (but is about to come on screen).
-
+       
     -----------------------------------------------------------------------------------------
 
-    elseif ( phase == "did" ) then
-        -- Called when the scene is now on screen.
-        -- Insert code here to make the scene come alive.
-        -- Example: start timers, begin animation, play audio, etc.
+    -- Called when the scene is now on screen.
+    -- Insert code here to make the scene come alive.
+    -- Example: start timers, begin animation, play audio, etc.
+    elseif ( phase == "did" ) then       
+        
+
     end
 
 end -- function scene:show( event )
@@ -149,7 +99,7 @@ function scene:hide( event )
         -- Called immediately after scene goes off screen.
     end
 
-end --function scene:hide( event )
+end -- function scene:hide( event )
 
 -----------------------------------------------------------------------------------------
 
@@ -159,14 +109,11 @@ function scene:destroy( event )
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
 
-    -----------------------------------------------------------------------------------------
-
-
     -- Called prior to the removal of scene's view ("sceneGroup").
     -- Insert code here to clean up the scene.
     -- Example: remove display objects, save state, etc.
 
-end --function scene:destroy( event )
+end -- function scene:destroy( event )
 
 -----------------------------------------------------------------------------------------
 -- EVENT LISTENERS
@@ -181,5 +128,3 @@ scene:addEventListener( "destroy", scene )
 -----------------------------------------------------------------------------------------
 
 return scene
-
-
